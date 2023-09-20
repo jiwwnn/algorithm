4b73707aa726f54f51bac942c44eab93fb55b247
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2023-09-20 13:55:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,6 +1,16 @@
+# def solution(s):
+#     dic = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+#     for k, v in dic.items():
+#         if k in s:
+#             s = s.replace(k, str(v))
+#     return int(s)
+
+
 def solution(s):
-    dic = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-    for i, v in dic.items():
-        if i in s:
-            s = s.replace(i, str(v))
+    en = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    dic = {k : v for k, v in zip(en, [i for i in range(10)]) }
+    for k, v in dic.items():
+        if k in s:
+            s = s.replace(k, str(v))
     return int(s)
+    
