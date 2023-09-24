6f397a0303feddc2a2639c8518ac527bd4643670
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2023-09-24 19:29:47 +0900

    [level 1] Title: 시저 암호, Time: 2.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,15 +1,3 @@
 def solution(s, n):
     answer = ''
-    for i in s:
-        if i == ' ':
-            answer += ' '
-            continue
-        if 65 <= ord(i) <= 90 and ord(i) + n > 90:
-            i = 65 + ((ord(i)+n)-90-1)
-        elif 97 <= ord(i) <= 122 and ord(i) + n > 122:    
-            i = 97 + ((ord(i)+n)-122-1)
-        else:        
-            i = ord(i) + n
-        answer += chr(i)
-    print(answer)
     return answer
