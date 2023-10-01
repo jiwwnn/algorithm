fb63311e4aacded848a6d062fdbc7a67e894a72b
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2023-10-01 19:19:04 +0900

    [level 0] Title: 공 던지기, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -2,7 +2,7 @@ def solution(numbers, k):
     idx = 1
     # k번째 던지는 사람 = k-1번째 사람이 토스한 상대
     for i in range(k-1):
-        # 두 칸씩 이동하므로 for문 2번 돌리기
+        # 두 칸씩 이동하므로 2번 반복
         for j in range(2):
             idx += 1
             # len(numbers)보다 클 시 -len(numbers)하여 맨 앞 인덱스로 돌아가기
