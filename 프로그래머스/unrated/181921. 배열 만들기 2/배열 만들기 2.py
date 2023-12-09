437c20ce808b3316118fa3121ac5ba5c4850d19b
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2023-12-09 16:48:07 +0900

    [level unrated] Title: 배열 만들기 2, Time: 28.83 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,20 @@
+def solution(l, r):
+    answer = []
+    num = '05'
+    for i in range(l, r+1):
+        count = 0
+        for j in str(i):
+            if j not in num: 
+                continue
+            count +=1
+        if count == len(str(i)):
+            answer.append(i)
+    
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
+    #     if str(i) not in num:
+    #         continue
+    #     else:
+    #         print(i)
+    # return answer
