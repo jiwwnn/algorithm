b78a67f8e9d4674b1e25736e4f4d4515f541f31d
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2024-01-27 19:38:44 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,10 @@
+#def solution(n_str):
+    #while n_str[0] == '0':
+        #n_str = n_str[1:]
+    #return n_str
+
+# def solution(n_str):
+#     return str(int(n_str))
+
+def solution(n_str):
+    return n_str.lstrip('0')
