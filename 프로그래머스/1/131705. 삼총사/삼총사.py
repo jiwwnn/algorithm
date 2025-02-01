90e62b2ac39915ec5e3985cebf9aa825b7823050
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2025-02-01 17:03:20 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -6,8 +6,7 @@ def count_triplets(number, index=0, count=0, total=0):
         return 0
 
     # 현재 숫자를 선택하는 경우와 선택하지 않는 경우로 나누어 탐색
-    return count_triplets(number, index + 1, count + 1, total + number[index]) + \
-           count_triplets(number, index + 1, count, total)
+    return count_triplets(number, index + 1, count + 1, total + number[index]) + count_triplets(number, index + 1, count, total)
 
 def solution(number):
     return count_triplets(number)
