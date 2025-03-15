9e372101bea066b059a18a434b2be874d9210345
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2025-03-15 17:45:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.88 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,12 +1,27 @@
+import heapq
+
 def solution(k, score):
+#     post = []
+#     honor = []
+    
+#     for i in score:
+#         honor.append(i)
+#         honor.sort() # 효율성 문제
+        
+#         if len(honor) > k:
+#             honor.pop(0)
+#         post.append(honor[0])
+#     return post
+
+
+    ## heapq 구조 활용
     post = []
     honor = []
     
     for i in score:
-        honor.append(i)
-        honor.sort()
+        heapq.heappush(honor, i)
         
         if len(honor) > k:
-            honor.pop(0)
+            heapq.heappop(honor)
         post.append(honor[0])
     return post
