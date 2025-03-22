4cbfddc3aae5b34d5d9f4bd064fc201b3f7324c6
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2025-03-22 21:06:38 +0900

    [level 1] Title: 콜라 문제, Time: 0.48 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -3,7 +3,7 @@ def solution(a, b, n):
     
     while n>0:
         free += n//a*b
-        n = (n//a)*b + n%a
+        n = n//a*b + n%a # 새로 얻은 콜라 + 남은 병
         
         if n < a:
             break
