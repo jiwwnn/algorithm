05d31a0dc10c58553e94a7b1dc06794b038b5cb5
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2025-05-17 21:27:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.96 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -6,3 +6,15 @@ def solution(participant, completion):
         if p != c:
             return p
     return participant[-1]
+
+
+# from collections import Counter
+
+# def solution(participant, completion):
+#     p_counter = Counter(participant) # 리스트에서 해당 요소가 몇 번 나왔는지 딕셔너리로 저장
+#                                      # {'leo': 1, 'kiki': 1, 'eden': 1}
+#     c_counter = Counter(completion)
+#                                      # {'eden': 1, 'kiki': 1}
+        
+#     answer = p_counter - c_counter   # {'leo': 1}
+#     return list(answer.keys())[0]
