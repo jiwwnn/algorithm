85e8b0138bd6e5bdf0bd474181409c171c427be9
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2025-05-31 18:49:37 +0900

    [level 1] Title: 문자열 나누기, Time: 3.04 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,46 @@
+# def solution(s):
+#     count = 0
+#     dic = {}
+
+#     for i, j in enumerate(s):        
+#         if j not in dic:
+#             dic[j] = 1
+#         else:
+#             dic[j] += 1
+            
+#         val = list(dic.values())
+#         if len(val) >= 2 and len(set(val)) == 1:
+#             count += 1
+#             dic = {}
+#             s = s[i+1:]
+            
+#         if i == len(s)-1:
+#             count += 1
+
+#     return count
+
+def solution(s):
+    count = 0
+    i = 0
+
+    while i < len(s):
+        x = s[i]
+        x_count = 0
+        other_count = 0
+
+        for j in range(i, len(s)):
+            if s[j] == x:
+                x_count += 1
+            else:
+                other_count += 1
+
+            if x_count == other_count:
+                count += 1
+                i = j + 1  # 다음 분리 시작점으로 이동
+                break
+        else:
+            # 끝까지 가도 개수가 같아지지 않으면 남은 부분 하나로 분리
+            count += 1
+            break
+
+    return count
